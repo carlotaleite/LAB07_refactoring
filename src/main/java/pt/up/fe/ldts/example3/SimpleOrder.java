5815d8fedfd96db29d8fcff801a8b595ef525d6d
carlotaleite <up202005428@edu.fe.up.pt>
2021-12-10 16:29:02 +0000

    First LAB07 commit

---
+++ src/main/java/pt/up/fe/ldts/example3/SimpleOrder.java
@@ -0,0 +1,19 @@
+package pt.up.fe.ldts.example3;
+
+public class SimpleOrder {
+    private Discount discount;
+    private double price;
+
+    public SimpleOrder(double price) {
+        this.price = price;
+    }
+
+    public void setDiscount(Discount discount) {
+        this.discount = discount;
+    }
+
+    public double getTotal() {
+        if (discount == null) return price;
+        return discount.applyDiscount(price);
+    }
+}
